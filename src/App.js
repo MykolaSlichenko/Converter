8de77af69e15f18e34bcc094029af14c9ba1799d
dell <nikolaiiislichenko@gmail.com>
2023-02-16 12:16:11 +0200

    add timeseries chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import CurrencyInput from './components/CurrencyInput/Currency-input';
 import axios from 'axios';
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 
 import './App.css';
 
@@ -10,17 +12,49 @@ function App() {
   const [currencyCurrent, setCurrencyCurrent] = useState('USD');
   const [currencyTarget, setCurrencyTarget] = useState('AUD');
   const [rates, setRates] = useState([]);
+  const [currenciesSeries, setCurrenciesSeries] = useState([]);
+  const [dataSeries, setDataSeries] = useState([]);
 
   const API_KEY = 'BMJFqnXMPMOSAqhVT8jGEbdpZ2zHjxLf';
-  const URL = 'https://api.apilayer.com/';
+  const URL = 'https://api.apilayer.com/fixer/';
+
+  const currenciesData = {
+    labels: dataSeries,
+    datasets: [
+      {
+        label: 'Time-Series Endpoint',
+        backgroundColor: 'red',
+        borderColor: 'red',
+        data: currenciesSeries,
+      },
+    ],
+  };
 
   useEffect(() => {
-    axios.get(`${URL}fixer/latest?base=USD&apikey=${API_KEY}`)
+    axios.get(`${URL}latest?base=USD&apikey=${API_KEY}`)
       .then(response => {
         setRates(response.data.rates);
       })
       .catch(() => {
         console.log('Error occured when fetching');
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get(`${URL}timeseries?apikey=${API_KEY}&start_date=2022-02-08&end_date=2023-02-08&base=USD`)
+      .then(response => {
+        const arrayCurrency = [];
+        const dataArray = [];
+        for (const [key, value] of Object.entries(response.data.rates)) {
+          // console.log(`${key}: ${value.currencyTarget}`);
+          arrayCurrency.push(value.AUD);
+          dataArray.push(key);
+        }
+        setCurrenciesSeries(arrayCurrency);
+        setDataSeries(dataArray);
+      })
+      .catch(() => {
+        console.log('Fail to fetch');
       });
   }, []);
 
@@ -36,22 +70,22 @@ function App() {
   const format = number => number.toFixed(5);
 
   const handleAmountFirstChange = (amountCurrent) => {
-    setAmountTarget(format(amountCurrent * rates[currencyTarget] / rates[currencyCurrent]));
+    setAmountTarget(format(parseInt(amountCurrent) * rates[currencyTarget] / rates[currencyCurrent]));
     setAmountCurrent(amountCurrent);
   };
 
   const handleCurrency1Change = (currencyCurrent) => {
-    setAmountTarget(format(amountCurrent * rates[currencyTarget] / rates[currencyCurrent]));
+    setAmountTarget(format(parseInt(amountCurrent) * rates[currencyTarget] / rates[currencyCurrent]));
     setCurrencyCurrent(currencyCurrent);
   };
 
   const handleAmountSecondChange = (amountTarget) => {
-    setAmountCurrent(format(amountTarget * rates[currencyCurrent] / rates[currencyTarget]));
+    setAmountCurrent(format(parseInt(amountTarget) * rates[currencyCurrent] / rates[currencyTarget]));
     setAmountTarget(amountTarget);
   };
 
   const handleCurrencyTargetChange = (currencyTarget) => {
-    setAmountCurrent(format(amountTarget) * rates[currencyCurrent] / rates[currencyTarget]);
+    setAmountCurrent(format(parseInt(amountTarget) * rates[currencyCurrent] / rates[currencyTarget]));
     setCurrencyTarget(currencyTarget);
   };
 
@@ -72,6 +106,7 @@ function App() {
         amount={amountTarget}
         currency={currencyTarget}
       />
+      <Line data={currenciesData} className='chart' />
     </div>
   );
 }
